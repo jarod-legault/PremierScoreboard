f39bc1fab4345fd16b9c8ebf61269f5d1e0b6535
Jarod Legault <jarod.legault@gmail.com>
2023-10-03 14:41:08 -0600

    Reduce width of score to not overlap notch.

--- components/TeamScore.tsx
+++ components/TeamScore.tsx
@@ -262,7 +262,7 @@ const styles = StyleSheet.create({
   },
   scoreContainer: {
     height: '90%',
-    width: '90%',
+    width: '83%',
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 20,
